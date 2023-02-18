2b27091d235679099b08be733dfc44eff8982d7d
Dustin Smith <Dustin.William.Smith@gmail.com>
2023-02-18 11:01:42 +0100

    use created credential

--- src/test/scala/com/growingintech/datasources/InputSourcesSpec.scala
+++ src/test/scala/com/growingintech/datasources/InputSourcesSpec.scala
@@ -36,7 +36,7 @@ class InputSourcesSpec extends AnyWordSpec with Matchers with BeforeAndAfterAll 
     .builder()
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-//    .config("credentialsFile", "/Users/dustin/Documents/code/Resume/web-resume/notebooks/ga-dustinsmith.json")
+    .config("credentialsFile", "/home/runner/work/spark-input-sources/spark-input-sources/gha-creds-26cf311093c8cb01.json")
     .appName("InputSource Test")
     .master("local[*]")
     .getOrCreate()
